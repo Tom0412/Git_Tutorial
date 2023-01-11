6deb8dd8c2218481925d236ac40e2dc031542ad2
TomKinjo <tom-kinjo-k22h@stu.kbc.ac.jp>
2023-01-11 09:36:45 +0900

    Update test01.cpp
    そーすこーどをしんぷるにした

--- test01/test01.cpp
+++ test01/test01.cpp
@@ -1,14 +1,7 @@
 #include<stdio.h>
 
-int main(void)
-{
-	int x;
-	scanf_s("%d", x);
-	switch (x)
-	{
-		printf("Hello Git World!\n");
-		return 0;
-	default:
-		break;
-	}
+int main(void) {
+
+	printf("Hello Git World!\n");
+	return 0;
 }
